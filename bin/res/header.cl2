ff7a823be13908c1e1a4f3091808cb3aefa68bd4
NetLamp <oplpublishergerman@gmail.com>
2016-10-13 19:36:16 +0200

    Operator update

--- bin/res/header.cl2
+++ bin/res/header.cl2
@@ -1,2 +1,1 @@
-#include ccl/std/_Environment_.cl2
 #include ccl/std/ops.cl2
--- src/csy/Operators.java
+++ src/csy/Operators.java
@@ -9,14 +9,18 @@ public class Operators {
 	private static HashMap<String, String> map = new HashMap<>();
 	
 	public static String lookup(String op) throws DebugException {
-		String res = map.get(op);
+		String res = map.get(op.trim());
 		if(res == null){
 			throw new DebugException("No such operator found: " + op);
 		}
 		return res;
 	}
 	
-	public static void add(String op, String func){
+	public static void add(String op, String func) throws DebugException{
+		String s = map.get(op.trim());
+		if(s != null){
+			throw new DebugException("Operator duplication: " + op);
+		}
 		map.put(op, func);
 	}
 	
