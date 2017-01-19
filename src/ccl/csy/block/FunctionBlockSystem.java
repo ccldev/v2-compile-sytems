4ea742746796bd6b3cea19a9dba182f60b10fc9b
NetLamp <oplpublishergerman@gmail.com>
2017-01-19 16:41:28 +0100

    extends xy
    def function(this) extends other{}

--- src/ccl/csy/block/FunctionBlockSystem.java
+++ src/ccl/csy/block/FunctionBlockSystem.java
@@ -15,11 +15,12 @@ import ccl.v2_1.err.ImplementationException;
 public class FunctionBlockSystem implements CompileSystem<CclCodeBlock, File> {
 
 	private static final Pattern DEF_PATTERN = Pattern.compile
-			("\\s*def\\s+([a-zA-Z0-9_]+)\\s*(\\[.+\\])?\\s*", Pattern.DOTALL);
+			("\\s*def\\s+([a-zA-Z0-9_]+)\\s*(extends\\s+[A-Za-z][A-Za-z0-9_]*)?\\s*", Pattern.DOTALL);
 	
 	protected static int count = 0;
 
 	protected String name;
+	private String extend;
 	
 	@Override
 	public boolean accept(CclCodeBlock infos) {
@@ -27,6 +28,7 @@ public class FunctionBlockSystem implements CompileSystem<CclCodeBlock, File> {
 		if(DEF_PATTERN.matcher(infos.getKeyword()).matches()){
 			count++;
 			this.name = extractName(infos.getKeyword());
+			this.extend = null;
 			return true;
 		}
 		return false;
@@ -40,7 +42,13 @@ public class FunctionBlockSystem implements CompileSystem<CclCodeBlock, File> {
 
 	@Override
 	public String compileComplete(CclCodeBlock infos)
-			throws ImplementationException, DebugException {
+			throws ImplementationException, DebugException {		
+		Pattern p = Pattern.compile("\\s*extends\\s+([A-Za-z][A-Za-z0-9_]*)\\s*");
+		Matcher m = p.matcher(infos.getAfterCondition());
+		if(m.matches()){
+			this.extend = m.group(1);
+		}
+		
 		return Parameters.parseParameters(infos.getCondition()) + "\n"
 			+ infos.getContent();
 	}
@@ -54,7 +62,24 @@ public class FunctionBlockSystem implements CompileSystem<CclCodeBlock, File> {
 	public String include() {
 		String path = getOutput().getPath();
 		path = path.substring(0, path.length() - 1) + "0";
-		return "reserve " + name + "\nputM " + path + "\nstore";
+		
+		StringBuilder b = new StringBuilder();
+		
+		b.append("reserve " + name + "\n");
+		
+		if(extend != null){
+			b.append("load " + extend);
+			b.append("\nget extend\n");
+		}
+		
+		b.append("putM " + path + "\n");
+		
+		if(extend != null){
+			b.append("invoke 1\n");
+		}
+		
+		b.append("store");
+		return b.toString();
 	}
 
 }
