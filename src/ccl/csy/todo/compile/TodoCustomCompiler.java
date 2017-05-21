d7303e4aa5dac93bf15a10a7bf98376f98b2cf0f
NetLamp <oplpublishergerman@gmail.com>
2017-05-21 18:48:11 +0200

    Every function is an operator now

--- src/ccl/csy/todo/compile/TodoCustomCompiler.java
+++ src/ccl/csy/todo/compile/TodoCustomCompiler.java
@@ -14,20 +14,49 @@ public class TodoCustomCompiler implements RawValueCompiler {
 			DebugException {
 		if(val.trim().isEmpty()) return "";
 		String op = "";
+
+		boolean abcOp = false;
+		loop:
 		for(int i = 0; i < val.length(); i++){
 			char c = val.charAt(i);
 			if(Constants.OPERATOR_CHARS.contains(c + "")){
 				op += c;
+			}else if(op.isEmpty() || abcOp){
+				if((c + "").trim().isEmpty()){
+					break loop;
+				}else{
+					op += c;
+					abcOp = true;
+				}
 			}else{
 				break;
 			}
 		}
 		String value = val.substring(op.length()).trim();
-		return buildResult(Operators.lookup(op), value.trim());
+		if(abcOp){
+			return buildResult(abcOp, op, value.trim());
+		}else{
+			return buildResult(abcOp, Operators.lookup(op), value.trim());
+		}
 	}
 
-	private String buildResult(String opMethod, String value) throws DebugException, ImplementationException {
+	private String buildResult(boolean abcOp, String opMethod, String value) throws DebugException, ImplementationException {
 		StringBuilder builder = new StringBuilder();
+
+		if(abcOp){
+			builder.append("load ");
+			builder.append(opMethod);
+			builder.append("\nget bind\n");
+			//operator.bind
+			builder.append("invoke1 1\n");
+			//operator.bind(a)
+			builder.append(StaticValueCompiler.compileValue(value));
+			builder.append("\ninvoke 1");
+			//operator.bind(a)(b)
+
+			return builder.toString();
+		}
+
 		builder.append("get ");
 		builder.append(opMethod);
 		builder.append("\n");
